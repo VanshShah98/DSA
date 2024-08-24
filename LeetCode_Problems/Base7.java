8876230e09d38209f0134b54636210ec58870216
VanshShah98 <vanshshah0908@gmail.com>
2024-08-25 00:21:03 +0530

    Q504 Base 7

--- LeetCode_Problems/Base7.java
+++ LeetCode_Problems/Base7.java
@@ -1,8 +1,12 @@
+/*
+ * Q504. Given an integer num, return a string of its base 7 representation.
+ */
 public class Base7 {
     public static void main(String[] args) {
         System.out.println(convertToBase7_2(-7));
     }
 
+    // Without using Integer.
     public static String convertToBase7(int num) {
         if (num < 0) {
             return "-" + convertToBase7(-num);
@@ -17,6 +21,7 @@ public class Base7 {
         return sb.reverse().toString();
     }
 
+    // Using Java Integer
     public static String convertToBase7_2(int num) {
         return Integer.toString(num, 7);
     }
