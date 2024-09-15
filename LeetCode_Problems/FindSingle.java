2da73142bd88f54075b246229ba5f071740e2fc4
VanshShah98 <vanshshah0908@gmail.com>
2024-09-15 23:34:56 +0530

    16th sept

---
+++ LeetCode_Problems/FindSingle.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class FindSingle {
+    public static void main(String[] args) {
+        int[] nums = { 1, 1, 2 };
+        System.out.print(singleNonDuplicate(nums));
+
+    }
+
+    public static int singleNonDuplicate(int[] nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ans = ans ^ nums[i];
+        }
+        return ans;
+    }
+}
---
+++ LeetCode_Problems/removereeatingelefromLinkedList.java
@@ -0,0 +1,47 @@
+public class removereeatingelefromLinkedList {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null)
+            return null;
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+
+        while (head != null) {
+            if (head.next != null && head.val == head.next.val) {
+
+                while (head.next != null && head.val == head.next.val) {
+                    head = head.next;
+                }
+                prev.next = head.next;
+            } else {
+                prev = prev.next;
+            }
+            head = head.next;
+        }
+
+        return dummy.next;
+    }
+}
