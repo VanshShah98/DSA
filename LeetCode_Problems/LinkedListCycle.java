353021f3acb8116f82bf180c009ad8e817057395
VanshShah98 <vanshshah0908@gmail.com>
2024-09-22 00:10:11 +0530

    22nd sept

---
+++ LeetCode_Problems/LinkedListCycle.java
@@ -0,0 +1,29 @@
+public class LinkedListCycle {
+    public static void main(String[] args) {
+
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ LeetCode_Problems/LinkedListCycle_II.java
@@ -0,0 +1,37 @@
+public class LinkedListCycle_II {
+    public static void main(String[] args) {
+
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode pos = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+        while (pos != slow) {
+            pos = pos.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+}
