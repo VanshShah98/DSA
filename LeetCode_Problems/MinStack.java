14e327b2c393e3d71ba94a7de5f92913d2e41b14
VanshShah98 <vanshshah0908@gmail.com>
2024-10-11 18:15:16 +0530

    11-10-2024

--- LeetCode_Problems/MinStack.java
+++ LeetCode_Problems/MinStack.java
@@ -5,6 +5,7 @@ class MinStack {
 
     public MinStack() {
         stack = new Stack<>();
+
     }
 
     public void push(int val) {
---
+++ LeetCode_Problems/datetoBinary.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class datetoBinary {
+    public static void main(String[] args) {
+        System.out.println(convertDateToBinary("1920-10-02"));
+
+    }
+
+    public static String convertDateToBinary(String date) {
+        StringBuilder sb = new StringBuilder();
+        for (String str : date.split("-")) {
+            sb.append(helper(str));
+            sb.append("-");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
+    }
+
+    public static String helper(String s) {
+        return Integer.toBinaryString(Integer.valueOf(s));
+    }
+}
