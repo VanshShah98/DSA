e1325695671ad49e44ec7c81623c1ce3c52f3e3f
VanshShah98 <vanshshah0908@gmail.com>
2024-12-04 17:03:44 +0530

    DLL Deleting

--- Linkedlist/Doublly_LinkedList/DLL.java
+++ Linkedlist/Doublly_LinkedList/DLL.java
@@ -30,6 +30,67 @@ public class DLL {
         System.out.println();
     }
 
+    public void deleteFirst() {
+        if (head == null) {
+            System.out.println("List is empty.");
+            return;
+        }
+
+        if (head == tail) { // Single node in the list
+            head = null;
+            tail = null;
+        } else {
+            head = head.next; // Move head to the next node
+            head.prev = tail; // Update the new head's `prev` to tail
+            tail.next = head; // Update tail's `next` to new head
+        }
+        size--;
+    }
+
+    public void deleteLast() {
+        if (head == null) {
+            System.out.println("List is empty.");
+            return;
+        }
+
+        if (head == tail) { // Single node in the list
+            head = null;
+            tail = null;
+        } else {
+            tail = tail.prev; // Move tail to the previous node
+            tail.next = head; // Update tail's `next` to head
+            head.prev = tail; // Update head's `prev` to the new tail
+        }
+        size--;
+    }
+
+    public void deleteAt(int index) {
+        if (index < 0 || index >= size) {
+            System.out.println("Index out of bounds.");
+            return;
+        }
+
+        if (index == 0) { // Deleting the first node
+            deleteFirst();
+            return;
+        }
+
+        if (index == size - 1) { // Deleting the last node
+            deleteLast();
+            return;
+        }
+
+        // Deleting a node at the middle
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next; // Traverse to the node at the given index
+        }
+
+        current.prev.next = current.next; // Link the previous node to the next node
+        current.next.prev = current.prev; // Link the next node to the previous node
+        size--;
+    }
+
     public void insert(int val) {
         Node newNode = new Node(val);
         newNode.data = val;
@@ -69,23 +130,6 @@ public class DLL {
         newNode.prev = curr;
         curr.next = newNode;
     }
-
-    // public void delete(int index) {
-    // if (head == null) {
-    // return;
-    // }
-    // Node curr = head;
-    // for (int i = 0; i < index - 1; i++) {
-    // curr = curr.next;
-    // }
-    // if (curr.next.next == null) {
-    // curr.next = null;
-    // return;
-    // }
-    // curr.next = curr.next.next;
-    // curr.next.next.prev = curr;
-    // size--;
-    // }
 
     private class Node {
         Node next;
